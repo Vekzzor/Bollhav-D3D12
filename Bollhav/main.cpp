32c7643bf784efb7e7c3913515d2eaa4d9fda5ae
Henrik Nilsson <henrik.n@live.se>
2019-02-14 09:33:19 +0100

    Structure changes

--- Bollhav/main.cpp
+++ Bollhav/main.cpp
@@ -1,15 +1,17 @@
-#include <Core/Window/Window.h>
 #include <Core/Render/Renderer.h>
+#include <Core/Window/Window.h>
+
 int main()
 {
 	Window window(VideoMode(), L"Hejsan");
+
 	Renderer r;
 	r.init(window.getSize().x, window.getSize().y, window.getHandle());
-	while (window.isOpen())
+
+	while(window.isOpen())
 	{
 		window.pollEvents();
 	}
 
 	return 0;
-
 }
